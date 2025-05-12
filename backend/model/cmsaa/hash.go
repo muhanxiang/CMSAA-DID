760340bfdf13efb66f4ecaa36901329c2383a423
muhanxiang <920365087@qq.com>
2025-05-12 11:45:03 +0800

    hash

---
+++ backend/model/cmsaa/hash.go
@@ -0,0 +1,25 @@
+package cmsaa
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"github.com/Nik-U/pbc"
+)
+
+type Hash struct {
+}
+
+func (hash *Hash) H0FromMessage(m string, pairing *pbc.Pairing) *pbc.Element {
+	rawHash := sha256.Sum256([]byte(m))
+	return pairing.NewG2().SetFromHash(rawHash[:])
+}
+
+func (hash *Hash) H1FromSplicedPks(head *pbc.Element, pks []*pbc.Element, pairing *pbc.Pairing) *pbc.Element {
+	var allBytes []byte
+	allBytes = append(allBytes, head.Bytes()...)
+	for _, pk := range pks {
+		allBytes = append(allBytes, pk.Bytes()...)
+	}
+	rawHash := sha512.Sum512(allBytes)
+	return pairing.NewZr().SetFromHash(rawHash[:])
+}
--- backend/model/cmsaa/system_pairing.go
+++ backend/model/cmsaa/system_pairing.go
@@ -3,7 +3,7 @@ package cmsaa
 import "github.com/Nik-U/pbc"
 
 type Pairing struct {
-	pairing *pbc.Pairing
+	Pairing *pbc.Pairing
 	G1      *pbc.Element
 	G2      *pbc.Element
 	H1      *pbc.Element
@@ -15,7 +15,7 @@ func Generate(rbits, qbits uint32) *Pairing {
 	params := pbc.GenerateA(rbits, qbits)
 	pairing := pbc.NewPairing(params)
 	return &Pairing{
-		pairing: pairing,
+		Pairing: pairing,
 		G1:      pairing.NewG1().Rand(),
 		G2:      pairing.NewG1().Rand(),
 		H1:      pairing.NewG2().Rand(),
@@ -24,5 +24,5 @@ func Generate(rbits, qbits uint32) *Pairing {
 }
 
 func (pairing *Pairing) Pair(e1, e2 *pbc.Element) *pbc.Element {
-	return pairing.pairing.NewGT().Pair(e1, e2)
+	return pairing.Pairing.NewGT().Pair(e1, e2)
 }
